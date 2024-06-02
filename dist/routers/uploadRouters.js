872b3e4f2ca9fca8d4ed205e9daa200fe2bc5436
codecreatorsss <codecreatorsss@gmail.com>
2024-06-03 01:54:27 +0530

    multer

--- dist/routers/uploadRouters.js
+++ dist/routers/uploadRouters.js
@@ -4,9 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const multer_1 = __importDefault(require("../utils/multer"));
+// import upload from "../utils/multer";
 const uploadControllers_1 = __importDefault(require("../controllers/uploadControllers"));
+const multer_1 = __importDefault(require("multer"));
 const uploadRouters = express_1.default.Router();
-uploadRouters.post("/", multer_1.default.single("file"), uploadControllers_1.default.uploadFile);
+const upload = (0, multer_1.default)({ storage: multer_1.default.memoryStorage() });
+uploadRouters.post("/", upload.single("file"), uploadControllers_1.default.uploadFile);
 exports.default = uploadRouters;
 //# sourceMappingURL=uploadRouters.js.map
--- src/routers/uploadRouters.ts
+++ src/routers/uploadRouters.ts
@@ -1,8 +1,10 @@
 import express from "express";
-import upload from "../utils/multer";
+// import upload from "../utils/multer";
 import uploadControllers from "../controllers/uploadControllers";
+import multer from "multer";
 
 const uploadRouters = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
 
 uploadRouters.post("/", upload.single("file"), uploadControllers.uploadFile);
 
